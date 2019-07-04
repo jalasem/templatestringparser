f5d5246bfa9d755b8de552a50a61e9771e706ca2
Abdulsamii Ajala <kgasta@gmail.com>
2019-07-04 12:01:27 +0100

    processMail function

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+ * TemplateString - Simple template strings
+ * Copyright (c) Abdulsamii Ajala (jalasem)
+ * https://github.com/jalasem/templateString/
+ *
+ * Released under the MIT license
+ * https://github.com/jalasem/templateString/blob/master/LICENSE
+ */
+
+/**
+ * 
+ * @param {string} template template string to be matched
+ * @param {object} values variables to be parsed
+ */
+const processMail = (template, values) => {
+  var processed = template
+
+  if (Array.isArray(values) || typeof values !== 'object') return processed
+
+  Object.keys(values).forEach(value => {
+    var re = new RegExp('{' + value + '}', "gm")
+    processed = processed.replace(re, values[value])
+  })
+  processed.replace(/\{.*?\}/gm, '')
+
+  return processed
+}
+
+module.exports = processMail
