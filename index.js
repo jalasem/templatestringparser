975d07dd2533c5ab7ff09fbc5b1146dc5a8c0b5e
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-07-24 00:51:32 +0100

    Support Deeply Nested Object Values

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ const processMail = (template, values, config = settings()) => {
 
   var processed = template
 
+  // flatten values object
+  values = flattenObj(values)
+
   if (Array.isArray(values) || typeof values !== 'object') return processed
 
   Object.keys(values).forEach(value => {
@@ -44,6 +47,33 @@ const processMail = (template, values, config = settings()) => {
  */
 const escapeRegExp = (regexp) => {
   return regexp.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+}
+
+/**
+ *
+ * @param {object} data deeply nested object OR an already flattened object
+ * @returns {object} flattened object
+ */
+const flattenObj = (data) => {
+  var result = {}
+  function recurse (cur, prop) {
+    if (Object(cur) !== cur) {
+      result[prop] = cur
+    } else if (Array.isArray(cur)) {
+      for(var i = 0, l = cur.length; i < l; i++)
+        recurse(cur[i], prop + "[" + i + "]")
+      if (l == 0) result[prop] = []
+    } else {
+      var isEmpty = true
+      for (var p in cur) {
+        isEmpty = false
+        recurse(cur[p], (prop) ? prop + "." + p : p)
+      }
+      if (isEmpty && prop) result[prop] = {}
+    }
+  }
+  recurse(data, "")
+  return result
 }
 
 /**
